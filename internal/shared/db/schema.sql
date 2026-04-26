2908fd8836a7ab446a8d74d26983e9e689617189
Alwin18 <alwinbezarius@gmail.com>
2026-04-26 13:38:51 +0700

    chore: update schema tabel and env key

--- internal/shared/db/schema.sql
+++ internal/shared/db/schema.sql
@@ -1,10 +1,36 @@
-CREATE TABLE IF NOT EXISTS users (
+CREATE TABLE IF NOT EXISTS roles (
     id SERIAL PRIMARY KEY,
     name VARCHAR(100) NOT NULL,
+    description TEXT,
+    status VARCHAR DEFAULT 'ACTIVE',
+    created_at TIMESTAMP WITH TIME ZONE DEFAULT NOW(),
+    updated_at TIMESTAMP WITH TIME ZONE DEFAULT NOW(),
+    deleted_at TIMESTAMP WITH TIME ZONE
+);
+
+CREATE TABLE IF NOT EXISTS "permissions" (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(100) NOT NULL,
+    description TEXT,
+    status VARCHAR DEFAULT 'ACTIVE',
+    created_at TIMESTAMP WITH TIME ZONE DEFAULT NOW(),
+    updated_at TIMESTAMP WITH TIME ZONE DEFAULT NOW(),
+    deleted_at TIMESTAMP WITH TIME ZONE
+);
+
+CREATE TABLE IF NOT EXISTS role_permissions (
+    role_id INTEGER NOT NULL REFERENCES "roles"(id) ON DELETE CASCADE,
+    permission_id INTEGER NOT NULL REFERENCES "permissions"(id) ON DELETE CASCADE,
+    PRIMARY KEY (role_id, permission_id)
+);
+
+CREATE TABLE IF NOT EXISTS users (
+    id SERIAL PRIMARY KEY,
+    username VARCHAR(100) NOT NULL,
     email VARCHAR(150) UNIQUE NOT NULL,
     password VARCHAR(255) NOT NULL,
-    role VARCHAR(50) DEFAULT 'user',
-    is_active BOOLEAN DEFAULT TRUE,
+    role_id INTEGER NULL REFERENCES roles(id) ON DELETE SET NULL,
+    status VARCHAR DEFAULT 'ACTIVE',
     created_at TIMESTAMP WITH TIME ZONE DEFAULT NOW(),
     updated_at TIMESTAMP WITH TIME ZONE DEFAULT NOW(),
     deleted_at TIMESTAMP WITH TIME ZONE
@@ -32,3 +58,12 @@ CREATE TABLE IF NOT EXISTS audit_logs (
 CREATE INDEX idx_users_email ON users(email);
 CREATE INDEX idx_refresh_tokens_user_id ON refresh_tokens(user_id);
 CREATE INDEX idx_audit_logs_user_id ON audit_logs(user_id);
+
+INSERT INTO "roles" (name, description, status) VALUES ('superadmin', 'Super Admin', 'ACTIVE');
+INSERT INTO "roles" (name, description, status) VALUES ('admin', 'Admin', 'ACTIVE');
+INSERT INTO "roles" (name, description, status) VALUES ('user', 'User', 'ACTIVE');
+
+INSERT INTO "permissions" (name, description, status) VALUES ('superadmin', 'Super Admin', 'ACTIVE');
+INSERT INTO "permissions" (name, description, status) VALUES ('admin', 'Admin', 'ACTIVE');
+INSERT INTO "permissions" (name, description, status) VALUES ('user', 'User', 'ACTIVE');
+
